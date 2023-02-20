42403da8cb0605c928dcc009318e45fee0a351f1
lejack92 <lejack921@gamil.com>
2023-02-20 23:11:44 +0900

    revise hamberger

--- common/common.js
+++ common/common.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', e => {
+    e.preventDefault();
     menu.classList.toggle('active');
 });
